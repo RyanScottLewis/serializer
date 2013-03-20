ceb90bf1521b9ee4f30e15697380149caa3d7e08
Ryan Scott Lewis <ryan@rynet.us>
2013-03-20 03:30:11 -0400

    Modulize interfaces

---
+++ lib/serializer/interfaces.rb
@@ -0,0 +1,8 @@
+module Serializer
+  
+  # The abstract class for serialization mechanism interface classes.
+  module Interfaces
+    
+  end
+  
+end
---
+++ lib/serializer/interfaces/marshal.rb
@@ -0,0 +1,26 @@
+require 'serializer/interface'
+
+module Serializer
+  module Interfaces
+    
+    # An interface for the Ruby Marshal class.
+    class Marshal < Interface
+      
+      # @param object The object to serialize.
+      # @param [Hash, #to_hash, #to_h] options The options to pass to the serializer's interface.
+      def dump(object, options={})
+        ::Marshal.dump(object)
+      end
+      
+      # @param object The object to deserialize.
+      # @param [Hash, #to_hash, #to_h] options The options to pass to the serializer's interface.
+      def load(object, options={})
+        ::Marshal.load(object)
+      end
+      
+      Interface.register(self)
+      
+    end
+  
+  end
+end
